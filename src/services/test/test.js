a4ed85489af932851a9aced9a49a7701c89691d7
Max <maxabcr2000@gmail.com>
2018-06-13 22:42:04 -0400

    Add custom service that using disallow hook that blocks all the external acces

---
+++ src/services/test/test.js
@@ -0,0 +1,54 @@
+const { disallow } = require('feathers-hooks-common');
+
+module.exports = function (app){
+  class MyService {
+    find(params) {
+      return Promise.resolve([]);
+    }
+    get(id, params) {
+      return Promise.resolve([]);
+    }
+    create(data, params) {
+        console.log("params from test.create: ", params);
+
+      return Promise.resolve({
+        text:'HaHaHa' + JSON.stringify(data)
+      });
+    }
+    update(id, data, params) {
+      return Promise.resolve([]);
+    }
+    patch(id, data, params) {
+      return Promise.resolve([]);
+    }
+    remove(id, params) {
+      return Promise.resolve([]);
+    }
+    setup(app, path) {
+      return Promise.resolve([]);
+    }
+  }
+    
+  app.use('/my-service', new MyService());
+
+  app.service('my-service').hooks({
+    before: {
+      all: [disallow('external')],
+      find: [],
+      get: [],
+      create: [],
+      update: [],
+      patch: [],
+      remove: []
+    },
+    after: {
+      all: [],
+      find: [],
+      get: [],
+      create: [],
+      update: [],
+      patch: [],
+      remove: []
+    }
+  });
+};
