b9f5b1c925875c3b42114969175aea01db8f7c7e
Max <maxabcr2000@gmail.com>
2018-06-25 02:21:28 -0400

    Use external sql file for postgres DB initialization

--- scripts/localtest.sh
+++ scripts/localtest.sh
@@ -3,18 +3,18 @@
 #setup
 docker run --rm -p 5432:5432 --name postgres -e POSTGRES_PASSWORD=123456 -d postgres
 sleep 5s
-docker exec -it postgres psql --port 5432 --username postgres --dbname postgres --command 'CREATE FUNCTION notify_trigger() RETURNS trigger AS $$ DECLARE BEGIN PERFORM pg_notify('"'"'watchers'"'"', jsonb_build_object('"'"'table_name'"'"' , TG_TABLE_NAME::text , '"'"'data'"'"' , row_to_json(NEW)::text , '"'"'operation'"'"' , TG_OP::text )::text);  RETURN new; END; $$ LANGUAGE plpgsql;'
+docker cp ./pginit.sql postgres:/pginit.sql
+docker exec postgres psql -U postgres -f /pginit.sql > /dev/null
 
 npm run start&
 
-sleep 5s
-docker exec -it postgres psql --port 5432 --username postgres --dbname postgres --command 'CREATE TRIGGER watched_table_trigger AFTER INSERT OR UPDATE OR DELETE ON messages FOR EACH ROW EXECUTE PROCEDURE notify_trigger();'
+pid=$!
 
 echo
 echo "feathers server started, press enter to stop and cleanup docker containers"
 echo
 
-pid="$(ps -a | grep node | awk '{print $1}')"
+
 echo $pid
 read -p "" x
 
---
+++ scripts/pginit.sql
@@ -0,0 +1,3 @@
+CREATE FUNCTION notify_trigger() RETURNS trigger AS $$ DECLARE BEGIN PERFORM pg_notify('watchers', jsonb_build_object('table_name' , TG_TABLE_NAME::text , 'data' , row_to_json(NEW)::text , 'operation' , TG_OP::text )::text);  RETURN new; END; $$ LANGUAGE plpgsql;
+CREATE TABLE messages (id serial NOT NULL primary key, content text, sender varchar(100), receiver varchar(100), createdAt timestamp default NULL);
+CREATE TRIGGER watched_table_trigger AFTER INSERT OR UPDATE OR DELETE ON messages FOR EACH ROW EXECUTE PROCEDURE notify_trigger();
